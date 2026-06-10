c9589f7f3fbd28b071d744af7dd48a27dfcb0531
igmerwina <igmerwina@gmail.com>
2026-06-10 09:31:24 +0700

    Remove elliptic runtime dependency

--- chain-util.js
+++ chain-util.js
@@ -1,18 +1,31 @@
 // berisi file untuk generate wallet key, unique hasesh etc
 
-const EC = require('elliptic').ec // import lib elictip curve algo  
+const crypto = require('crypto')
 const SHA256 = require('crypto-js/sha256')
-const uuidV1 = require('uuid/v1')
-const ec = new EC('secp256k1') // implementasi jenis algo elictip pada bitcoin
 
  
 class ChainUtil{
     static genKeyPair(){
-        return ec.genKeyPair()
+        const { privateKey, publicKey } = crypto.generateKeyPairSync('ec', {
+            namedCurve: 'secp256k1'
+        })
+
+        return {
+            getPublic(){
+                return {
+                    encode(){
+                        return publicKey.export({ type: 'spki', format: 'der' }).toString('hex')
+                    }
+                }
+            },
+            sign(dataHash){
+                return crypto.sign('SHA256', Buffer.from(dataHash), privateKey).toString('hex')
+            }
+        }
     }
 
     static id(){
-        return uuidV1()
+        return crypto.randomUUID()
     }
 
     static hash(data){
@@ -20,7 +33,13 @@ class ChainUtil{
     }
 
     static verifySignature(publicKey, signature, dataHash){
-        return ec.keyFromPublic(publicKey, 'hex').verify(dataHash, signature)
+        const key = crypto.createPublicKey({
+            key: Buffer.from(publicKey, 'hex'),
+            format: 'der',
+            type: 'spki'
+        })
+
+        return crypto.verify('SHA256', Buffer.from(dataHash), key, Buffer.from(signature, 'hex'))
     }
 }
 
